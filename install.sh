99733bf9a741b0e956a98c4c60af1c97f6a63905
HotFish <admin@hot.fish>
2024-10-16 17:17:38 -0400

    modify installer to properly ignore certain files

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 # Make sure that dotfiles are fully here
 git submodule update --init --recursive
 
+# Make sure that we can change necessary files without checking
+# changes into git
+
+git update-index --skip-worktree hardware-configuration.nix
+git update-index --skip-worktree box.nix
+
 #sudo rm hardware-configuration.nix
 sudo nixos-generate-config --show-hardware-config > ./hardware-configuration.nix
 
