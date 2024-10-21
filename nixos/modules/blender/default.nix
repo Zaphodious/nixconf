2cac3ea1570f126ca713a0220d396a0a1d40fbae
HotFish <admin@hot.fish>
2024-10-21 09:49:57 -0400

    fix weirdness with blender thing

--- nixos/modules/blender/default.nix
+++ nixos/modules/blender/default.nix
@@ -24,11 +24,7 @@
   config = lib.mkIf config.modules.blender.enabled {
 
 
-<<<<<<< HEAD
     environment.systemPackages = with pkgs; [
-=======
-    environment.systemPackages = with pkgs-unstable; [
->>>>>>> a0c848d9c9d131bad61a23c218ab0cddbfd9b134
       # ... other packages
       blender
     ];
